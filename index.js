2756bf861feb3c5502b1f539a8323a93cb1ac302
Brian <wamwangimathenge@gmail.com>
2022-11-21 20:38:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const num5 = 12;
+const num6 = 7;
+const num7 = 20;
+const num8 = 1;
+const num9 = 16;
+const max = Math.max(num5, num6, num7, num8, num9);
+
+const random = Math.floor(Math.random()) + 1;
